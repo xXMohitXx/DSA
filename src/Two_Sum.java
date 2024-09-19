228a5d990852f2f0286910853d148e6214db6b73
mohit <molimanglani756@gmail.com>
2024-09-19 22:47:56 +0530

    commit

---
+++ src/Two_Sum.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Two_Sum {
+    public int[] twoSum(int[] nums, int target) {
+        int n = nums.length;
+        for(int i = 0 ; i < n-1 ; i++){
+            for(int j = i + 1 ; j < n ; j++){
+                if(nums[i]+nums[j] == target){
+                    return new int[]{i,j};
+                }
+            }
+        }
+        return new int[]{};
+    }
+}
